dc3b34654ba4f69d2296ad53cdce690ba65e0549
Mark Meretzky <mark.meretzky@gmail.com>
2018-12-28 09:34:45 -0500

    Update ViewController.swift

--- LifeCycle/ViewController.swift
+++ LifeCycle/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("\(type(of: self)) viewDidLoad");
         if let nibName: String = nibName {
-            print("nibName = \(nibName)");
+            print("nibName = \(nibName)");   //Where was the view loaded from?
         }
     }
     

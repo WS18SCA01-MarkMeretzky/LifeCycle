c0acbbd49b9c240e5402263f3eafc1f03d27c7ae
Mark Meretzky <mark.meretzky@gmail.com>
2019-01-31 15:51:48 -0500

    Update ViewController.swift

--- LifeCycle/ViewController.swift
+++ LifeCycle/ViewController.swift
@@ -14,9 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
         print("\(type(of: self)) viewDidLoad");
-        if let nibName: String = nibName {
-            print("nibName = \(nibName)");   //Where was the view loaded from?
-        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
